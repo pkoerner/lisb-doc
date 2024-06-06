d12a87195e93fe0ba4add9cef785d34d6c79b3b7
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2024-06-06 14:49:12 +0200

    muchos updatos

---
+++ content/md/docs/lisb.md
@@ -0,0 +1,8 @@
+{:title "Welcome"
+ :layout :page
+ :page-index 2}
+
+This is the page where I talk about B and lisb.
+
+Start here:
+
--- src/cryogen/core.clj
+++ src/cryogen/core.clj
@@ -7,12 +7,25 @@
   (:use [lisb.translation.util])
   )
 
+(defn group-docs-by-section [pages]
+  (->> pages
+       (map #(select-keys % [:title :uri :page-index :section]))
+       (group-by :section)
+       (map (fn [[section pages]]
+              {:section section
+               :pages   (map #(select-keys % [:title :uri]) pages)}))))
+
+
 (defn -main []
   (println :hello)
   (load-plugins)
   (cryogen-core.compiler/compile-assets-timed
      {:update-article-fn #_:postprocess-article-html-fn
       replace-lisb-blocks
+      :extend-params-fn
+      (fn extend-params [params {:keys [pages]}]
+        (assoc params :sections
+                      (group-docs-by-section pages)))
       })
   (System/exit 0))
 
