b38f2a2ec83c85dd6eee086fe28434a22624930e
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2024-06-07 15:11:31 +0200

    add substitutions, adapt postprocessor

---
+++ content/md/docs/substitutions.md
@@ -0,0 +1,59 @@
+{:title "Substitutions"
+ :layout :page
+ :page-index 50
+ :navbar? false
+ :section "B Syntax Overview"
+ :toc true}
+
+## Empty Assignment
+
+```pplisb
+skip
+```
+
+## Assignments
+
+```pplisb
+(assign :x 42)
+(assign :x 42 :y 1337)
+```
+
+```pplisb
+(becomes-element-of [:x] nat-set))
+(becomes-member [:x :y] (cartesian-product nat-set (range 1 10)))
+```
+
+```pplisb
+(becomes-such [:x :y] (= :x (inc :y)))
+```
+
+```pplisb
+(<-- [:x :y] (op-call :foo :a 42))
+```
+
+## Composition
+
+```pplisb
+(|| (assign :x 42) (assign :y 43) (assign :z 1337))
+(parallel-sub (assign :x 42) (assign :y 43) (assign :z 1337))
+(sequential-sub (assign :x 42) (assign :y 43) (assign :z 1337))
+```
+
+## Choices, Bindings, ...
+
+```pplisb
+(any [:x :y] (< min-int :x :y max-int) (assign :a :x :b :y))
+(let-sub [:x 42 :y 1337] (assign :a :x :y 1337))
+(var-sub [:x] (assign :x 42) (assign :a :x))
+(pre (= :x 42) (assign :y 42))
+(assert (= :x 42) (assign :y 42))
+(choice (assign :x 42) (assign :y 42))
+(if-sub (= :x 0) (assign :x 100))
+(if-sub (= :x 0) (assign :x 100) (assign :x (dec :x)))
+(cond (= :x 0) (assign :x 1) (= :y 0) (assign :y 1))
+(cond (= :x 0) (assign :x 1) (= :y 0) (assign :y 1) (assign :v "default"))
+(select (= :x 0) (assign :x 1) (= :y 0) (assign :y 1))
+(select (= :x 0) (assign :x 1) (= :y 0) (assign :y 1) (assign :v "default"))
+(case :x 0 (assign :x 1) 1 (assign :y 1))
+(case :x 0 (assign :x 1) 1 (assign :y 1) (assign :v "default"))
+```
--- src/cryogen/postprocessor.clj
+++ src/cryogen/postprocessor.clj
@@ -18,15 +18,23 @@
        (map repl (read-string (str \[ (first content-field) \])))))
 
 (defn repl-b [form]
-  #_(println :XXX/b form)
   (str "B => " (str form) \newline
        (str (try (lc/eval-ir-formula (b-expression->ir form))
                  (catch Exception e "lol, broken"))) \newline) )
 
 (defn get-forms-b [content-field]
-  #_(println content-field)
   (apply str 
        (map repl-b (clojure.string/split-lines (first content-field)))))
+
+
+(defn lisb-b' [form]
+  (str "lisb=> " (str form) \newline
+       (str (try (lisb->b (read-string form))
+                 (catch Exception e "lol, broken"))) \newline) )
+
+(defn lisb-b [content-field]
+  (apply str 
+       (map lisb-b' (clojure.string/split-lines (first content-field)))))
 
 (defn replace-lisb-blocks
   [article _params]
@@ -35,14 +43,17 @@
   (update article
           :content-dom
           (fn [content-dom]
-            (let [res  (s/transform [(s/walker #(and (map? %) (= (:tag %) :code) (= "lisb" (get-in % [:attrs :class]))))]
+            (let [res (->> content-dom  
+                       (s/transform [(s/walker #(and (map? %) (= (:tag %) :code) (= "lisb" (get-in % [:attrs :class]))))]
                                     (fn [m] (-> m 
-                                                (assoc-in [:attrs :class] "clojure") (update :content get-forms)))
-                                    content-dom)
-                  res (s/transform [(s/walker #(and (map? %) (= (:tag %) :code) (= "b-expr" (get-in % [:attrs :class]))))]
+                                                (assoc-in [:attrs :class] "clojure") (update :content get-forms))))
+                       (s/transform [(s/walker #(and (map? %) (= (:tag %) :code) (= "b-expr" (get-in % [:attrs :class]))))]
                                     (fn [m] (-> m 
-                                                (assoc-in [:attrs :class] "B") (update :content get-forms-b)))
-                                    res)
+                                                (assoc-in [:attrs :class] "B") (update :content get-forms-b))))
+                       (s/transform [(s/walker #(and (map? %) (= (:tag %) :code) (= "pplisb" (get-in % [:attrs :class]))))]
+                                    (fn [m] (-> m 
+                                                (assoc-in [:attrs :class] "clojure") (update :content lisb-b)))
+                                    ))
                   ]
               #_(println res)
               res
